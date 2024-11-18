deb8ddd8d7978e27b82957b42a8f2e62cfb7d9ff
Duong <ntduong071200@gmail.com>
2024-11-18 23:13:09 +0700

    Add UTs for Bishop and Pawn Movement

--- lib/src/test/java/com/demo/chessboard/service/impl/BishopMovementTest.java
+++ lib/src/test/java/com/demo/chessboard/service/impl/BishopMovementTest.java
@@ -1,4 +1,77 @@
 package com.demo.chessboard.service.impl;
 
-public class BishopMovementTest {
+import com.demo.chessboard.entity.Bishop;
+import com.demo.chessboard.entity.Pawn;
+import com.demo.chessboard.entity.base.Piece;
+import com.demo.chessboard.entity.base.Position;
+import com.demo.chessboard.enums.File;
+import com.demo.chessboard.enums.Rank;
+import com.demo.chessboard.enums.Side;
+import com.demo.chessboard.service.ChessBoard;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class BishopMovementTest {
+
+    private ChessBoard board;
+    private Bishop bishop;
+
+    @BeforeEach
+    public void setUp() {
+        board = new ChessBoard();
+        Position position = Position.builder().file(File.D).rank(Rank.R4).build();
+        bishop = new Bishop( Side.WHITE, position);
+    }
+
+    @Test
+    void testCalculateAvailableMoves_OpenPath() {
+        // Given: Bishop at D4
+        board.addPiece(bishop);
+
+        // When: Calculate available moves for bishop
+        Set<Position> availableMoves = new BishopMovement().calculateAvailableMoves(board, bishop);
+
+        // Then: The bishop should have available moves along diagonals
+        assertTrue(availableMoves.contains(new Position(File.E, Rank.R5)));
+        assertTrue(availableMoves.contains(new Position(File.C, Rank.R3)));
+        assertTrue(availableMoves.contains(new Position(File.B, Rank.R2)));
+        assertTrue(availableMoves.contains(new Position(File.A, Rank.R1)));
+    }
+
+    @Test
+    void testCalculateAvailableMoves_BlockingFriendlyPiece() {
+        // Given: Bishop at D4 and a friendly piece at E5
+        board.addPiece(bishop);
+
+        Position friendlyPiecePosition = Position.builder().file(File.E).rank(Rank.R5).build();
+        Piece friendlyPiece = new Pawn(Side.WHITE, friendlyPiecePosition);
+        board.addPiece(friendlyPiece);
+
+        // When: Calculate available moves for bishop
+        Set<Position> availableMoves = new BishopMovement().calculateAvailableMoves(board, bishop);
+
+        // Then: The bishop should not be able to move to E5 because it's blocked by a friendly piece
+        assertFalse(availableMoves.contains(new Position(File.E, Rank.R5)));
+    }
+
+    @Test
+    void testCalculateAvailableMoves_CapturingOpponentPiece() {
+        // Given: Bishop at D4 and an opponent's piece at E5
+        board.addPiece(bishop);
+
+        Position oponentPiecePosition = Position.builder().file(File.E).rank(Rank.R5).build();
+        Piece opponentPiece = new Pawn( Side.BLACK, oponentPiecePosition);
+        board.addPiece(opponentPiece);
+
+        // When: Calculate available moves for bishop
+        Set<Position> availableMoves = new BishopMovement().calculateAvailableMoves(board, bishop);
+
+        // Then: The bishop should be able to capture the opponent's piece at E5
+        assertTrue(availableMoves.contains(new Position(File.E, Rank.R5)));
+    }
 }
--- lib/src/test/java/com/demo/chessboard/service/impl/PawnMovementTest.java
+++ lib/src/test/java/com/demo/chessboard/service/impl/PawnMovementTest.java
@@ -1,4 +1,131 @@
 package com.demo.chessboard.service.impl;
 
-public class PawnMovementTest {
+import com.demo.chessboard.entity.Pawn;
+import com.demo.chessboard.entity.base.Piece;
+import com.demo.chessboard.entity.base.Position;
+import com.demo.chessboard.enums.File;
+import com.demo.chessboard.enums.Rank;
+import com.demo.chessboard.enums.Side;
+import com.demo.chessboard.service.ChessBoard;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class PawnMovementTest {
+
+    private ChessBoard board;
+    private Pawn whitePawn;
+    private Pawn blackPawn;
+
+    @BeforeEach
+    void setUp() {
+        board = new ChessBoard();
+        Position whitePawnPosition = Position.builder().file(File.E).rank(Rank.R2).build();
+        Position blackPawnPosition = Position.builder().file(File.E).rank(Rank.R7).build();
+        whitePawn = new Pawn(Side.WHITE, whitePawnPosition);
+        blackPawn = new Pawn(Side.BLACK, blackPawnPosition);
+    }
+
+    @Test
+    void testCalculateAvailableMoves_WhitePawn_MoveForward() {
+        // Given: White pawn at E4
+        Position whitePawnPosition = Position.builder().file(File.E).rank(Rank.R4).build();
+        Piece movedWhitePawn = new Pawn(Side.WHITE, whitePawnPosition);
+        board.addPiece(movedWhitePawn);
+
+        // When: Calculate available moves for white pawn
+        Set<Position> availableMoves = new PawnMovement().calculateAvailableMoves(board, movedWhitePawn);
+
+        // Then: White pawn should have the option to move to E5
+        assertTrue(availableMoves.contains(new Position(File.E, Rank.R5)));
+    }
+
+    @Test
+    void testCalculateAvailableMoves_WhitePawn_DoubleMoveFromStart() {
+        // Given: White pawn at E2
+        board.addPiece(whitePawn);
+
+        // When: Calculate available moves for white pawn
+        Set<Position> availableMoves = new PawnMovement().calculateAvailableMoves(board, whitePawn);
+
+        // Then: White pawn should have the option to move two squares ahead to E4
+        assertTrue(availableMoves.contains(new Position(File.E, Rank.R4)));
+    }
+
+    @Test
+    void testCalculateAvailableMoves_BlackPawn_MoveForward() {
+        // Given: Black pawn at E7
+        board.addPiece(blackPawn);
+
+        // When: Calculate available moves for black pawn
+        Set<Position> availableMoves = new PawnMovement().calculateAvailableMoves(board, blackPawn);
+
+        // Then: Black pawn should have the option to move to E6
+        assertTrue(availableMoves.contains(new Position(File.E, Rank.R6)));
+    }
+
+    @Test
+    void testCalculateAvailableMoves_BlackPawn_DoubleMoveFromStart() {
+        // Given: Black pawn at E7
+        board.addPiece(blackPawn);
+
+        // When: Calculate available moves for black pawn
+        Set<Position> availableMoves = new PawnMovement().calculateAvailableMoves(board, blackPawn);
+
+        // Then: Black pawn should have the option to move two squares ahead to E5
+        assertTrue(availableMoves.contains(new Position(File.E, Rank.R5)));
+    }
+
+    @Test
+    void testCalculateAvailableMoves_CaptureWhitePawn() {
+        // Given: White pawn at E3 and a black pawn at D4
+        Position whitePawnPosition = Position.builder().file(File.E).rank(Rank.R3).build();
+        Position blackPawnPosition = Position.builder().file(File.D).rank(Rank.R4).build();
+        whitePawn = new Pawn(Side.WHITE, whitePawnPosition);
+        blackPawn = new Pawn(Side.BLACK, blackPawnPosition);
+        board.addPiece(whitePawn);
+        board.addPiece(blackPawn);
+
+        // When: Calculate available moves for the white pawn at E3
+        Set<Position> availableMoves = new PawnMovement().calculateAvailableMoves(board, whitePawn);
+        availableMoves.stream().forEach(move -> System.out.println(move.toString()));
+
+        // Then: White pawn should be able to capture the black pawn at D4
+        assertTrue(availableMoves.contains(new Position(File.D, Rank.R4)));
+    }
+
+    @Test
+    void testCalculateAvailableMoves_CaptureBlackPawn() {
+        // Given: Black pawn at E6 and a white pawn at F5
+        Position whitePawnPosition = Position.builder().file(File.F).rank(Rank.R5).build();
+        Position blackPawnPosition = Position.builder().file(File.E).rank(Rank.R6).build();
+        whitePawn = new Pawn(Side.WHITE, whitePawnPosition);
+        blackPawn = new Pawn(Side.BLACK, blackPawnPosition);
+        board.addPiece(whitePawn);
+        board.addPiece(blackPawn);
+
+        // When: Calculate available moves for the black pawn at E6
+        Set<Position> availableMoves = new PawnMovement().calculateAvailableMoves(board, blackPawn);
+
+        // Then: Black pawn should be able to capture the white pawn at F5
+        assertTrue(availableMoves.contains(new Position(File.F, Rank.R5)));
+    }
+
+    @Test
+    void testCalculateAvailableMoves_BlockingPiece() {
+        // Given: White pawn at E2 and a piece blocking at E3
+        board.addPiece(whitePawn);
+        Position whitePawnPosition = Position.builder().file(File.E).rank(Rank.R3).build();
+        board.addPiece(new Pawn(Side.WHITE, whitePawnPosition));
+
+        // When: Calculate available moves for white pawn
+        Set<Position> availableMoves = new PawnMovement().calculateAvailableMoves(board, whitePawn);
+
+        // Then: White pawn should not be able to move to E3 due to blocking piece
+        assertFalse(availableMoves.contains(new Position(File.E, Rank.R3)));
+    }
 }
