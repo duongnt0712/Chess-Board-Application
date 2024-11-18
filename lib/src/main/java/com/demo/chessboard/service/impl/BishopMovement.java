90831e0433b2612893f5707f8d9eaf1f48bcf4b4
Duong <ntduong071200@gmail.com>
2024-11-18 23:13:32 +0700

    Implement Bishop and Pawn Movement

--- lib/src/main/java/com/demo/chessboard/service/impl/BishopMovement.java
+++ lib/src/main/java/com/demo/chessboard/service/impl/BishopMovement.java
@@ -2,14 +2,41 @@ package com.demo.chessboard.service.impl;
 
 import com.demo.chessboard.entity.base.Piece;
 import com.demo.chessboard.entity.base.Position;
+import com.demo.chessboard.enums.File;
+import com.demo.chessboard.enums.Rank;
 import com.demo.chessboard.service.ChessBoard;
 import com.demo.chessboard.service.MovementService;
 
+import java.util.HashSet;
 import java.util.Set;
 
 public class BishopMovement implements MovementService {
     @Override
     public Set<Position> calculateAvailableMoves(ChessBoard board, Piece piece) {
-        return Set.of();
+        Set<Position> moves = new HashSet<>();
+        Position currentPosition = piece.getPosition();
+
+        int[][] directions = {{1, 1}, {1, -1}, {-1, 1}, {-1, -1}};
+        for (int[] direction : directions) {
+            int file = currentPosition.file().getValue();
+            int rank = currentPosition.rank().getValue();
+
+            while (true) {
+                file += direction[0];
+                rank += direction[1];
+                if (file < 1 || rank < 1 || file > 8 || rank > 8 ) break;
+                Position pos = new Position(File.toFile(file), Rank.toRank(rank));
+                if (!board.isValidPosition(pos)) break;
+                if (board.isEmpty(pos)) {
+                    moves.add(pos);
+                } else {
+                    if (board.isOccupiedByOpponent(pos, piece.getSide())) {
+                        moves.add(pos);
+                    }
+                    break;
+                }
+            }
+        }
+        return moves;
     }
 }
--- lib/src/main/java/com/demo/chessboard/service/impl/PawnMovement.java
+++ lib/src/main/java/com/demo/chessboard/service/impl/PawnMovement.java
@@ -2,6 +2,9 @@ package com.demo.chessboard.service.impl;
 
 import com.demo.chessboard.entity.base.Piece;
 import com.demo.chessboard.entity.base.Position;
+import com.demo.chessboard.enums.File;
+import com.demo.chessboard.enums.Rank;
+import com.demo.chessboard.enums.Side;
 import com.demo.chessboard.service.ChessBoard;
 import com.demo.chessboard.service.MovementService;
 
@@ -13,7 +16,34 @@ public class PawnMovement implements MovementService {
     @Override
     public Set<Position> calculateAvailableMoves(ChessBoard board, Piece piece) {
         Set<Position> moves = new HashSet<>();
-        moves.add(new Position(piece.getPosition().file(), piece.getPosition().rank())); // Add rank 1 step ahead
+        Position current = piece.getPosition();
+        int direction = piece.getSide() == Side.WHITE ? 1 : -1;
+
+        // Forward move
+        Position forward = current.toBuilder().rank(Rank.toRank(current.rank().getValue() + direction)).build();
+        if (board.isEmpty(forward)) {
+            moves.add(forward);
+
+            // Double move from initial position
+            if ((piece.getSide() == Side.WHITE && current.rank().getValue() == 2) || (piece.getSide() == Side.BLACK && current.rank().getValue() == 7)) {
+                Position doubleForward = new Position( current.file(), Rank.toRank(current.rank().getValue() + 2 * direction));
+                if (board.isEmpty(doubleForward)) {
+                    moves.add(doubleForward);
+                }
+            }
+        }
+
+        // Captures
+        for (int dx : new int[]{-1, 1}) {
+            int newFileValue = current.file().getValue() + dx;
+            int newRankValue = current.rank().getValue() + direction;
+            if (newFileValue >= 1 && newFileValue <= 8 && newRankValue >= 1 && newRankValue <= 8) {
+                Position diagonal = new Position(File.toFile(newFileValue), Rank.toRank(newRankValue));
+                if (board.isOccupiedByOpponent(diagonal, piece.getSide())) {
+                    moves.add(diagonal);
+                }
+            }
+        }
         return moves;
     }
 }
