0a76731645cc76fa25dc758e94145fb5e411a01b
Duong <ntduong071200@gmail.com>
2024-11-18 23:12:38 +0700

    Implement ChessBoard

--- lib/src/main/java/com/demo/chessboard/service/ChessBoard.java
+++ lib/src/main/java/com/demo/chessboard/service/ChessBoard.java
@@ -2,20 +2,48 @@ package com.demo.chessboard.service;
 
 import com.demo.chessboard.entity.base.Piece;
 import com.demo.chessboard.entity.base.Position;
-import com.demo.chessboard.exceptions.InvalidPositionException;
+import com.demo.chessboard.enums.Side;
 
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
 
 public class ChessBoard {
-    private final Map<Position, Piece> board = new HashMap<>();
+    private static final int BOARD_SIZE = 8;
+    private final Map<Position, Piece> board;
+
+    public ChessBoard() {
+        this.board = new HashMap<>();
+    }
+
+    public boolean isValidPosition(Position position) {
+        return position.file().getValue() >= 0 && position.file().getValue() < BOARD_SIZE &&
+                position.rank().getValue() >= 0 && position.rank().getValue() < BOARD_SIZE;
+    }
+
+    public boolean isEmpty(Position position) {
+        return board.get(position) == null;
+    }
+
+    public boolean isOccupiedByOpponent(Position position, Side side) {
+        Piece piece = board.get(position);
+        return piece != null && piece.getSide() != side;
+    }
 
     public void addPiece(Piece piece) {
-        if (board.containsKey(piece.getPosition())) {
-            throw new InvalidPositionException(piece.getPosition());
-        }
         board.put(piece.getPosition(), piece);
+    }
+
+    public void movePiece(Position from, Position to) {
+        Piece piece = board.remove(from);
+        if (piece != null) {
+            piece.setPosition(to);
+            board.put(to, piece);
+        }
+    }
+
+    public Piece getPiece(Position position) {
+        return board.get(position);
     }
 
     public Set<Piece> getAllPieces() {
